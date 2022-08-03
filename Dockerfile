201d59b156e33ec511c8a8682a4be748bb83624f
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-08-03 13:00:37 +0300

    AddedDocker

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,23 @@
 FROM python:3.10.4-slim-buster
-LABEL maintainer="serhiy.zhyhadlo@gmail.com"
+LABEL mainrainer="serhiy.zhyhadlo@gmail.com"
 
 ENV PYTHONUNBUFFERED 1
 
-WORKDIR app/
+WORKDIR restaurant/
 
 COPY requirements.txt requirements.txt
-
-RUN apt-get update\
-    && apt-get -y install libpq-dev gcc
-
 RUN pip install -r requirements.txt
 
 COPY . .
 
-RUN adduser \
-    --disabled-password\
-    --no-create-home\
+RUN mkdir -p /vol/web/media
+
+RUN adduser  \
+    --disabled-password  \
+    --no-create-home  \
     django-user
 
 RUN chown -R django-user:django-user /vol/
 RUN chmod -R 755 /vol/web/
-RUN mkdir -p /vol/web/media
 
 USER django-user
--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,13 +78,12 @@ AUTH_USER_MODEL = "user.Employee"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restaurant',
-        'USER': 'postgres',
-        'PASSWORD': '1962639196',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["DB_HOST"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
     }
 }
 
